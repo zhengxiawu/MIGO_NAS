1ccb45ca1fa1f8c2932bf3d84042a7f670cf5b93
Xiawu zheng <zhengxiawu@126.com>
2019-11-20 09:55:03 +0800

    init

---
+++ config/config.py
@@ -0,0 +1,94 @@
+import argparse
+import os
+from functools import partial
+import torch
+import time
+
+
+def get_parser(name):
+    """ make default formatted parser """
+    parser = argparse.ArgumentParser(name, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    # print default value always
+    parser.add_argument = partial(parser.add_argument, help=' ')
+    return parser
+
+
+def parse_gpus(gpus):
+    if gpus == 'all':
+        return list(range(torch.cuda.device_count()))
+    else:
+        return [int(s) for s in gpus.split(',')]
+
+
+class BaseConfig(argparse.Namespace):
+    def print_params(self, prtf=print):
+        prtf("")
+        prtf("Parameters:")
+        for attr, value in sorted(vars(self).items()):
+            prtf("{}={}".format(attr.upper(), value))
+        prtf("")
+
+    def as_markdown(self):
+        """ Return configs as markdown format """
+        text = "|name|value|  \n|-|-|  \n"
+        for attr, value in sorted(vars(self).items()):
+            text += "|{}|{}|  \n".format(attr, value)
+
+        return text
+
+
+def dataset_parser(parser):
+    parser.add_argument('--dataset', required=False, default='CIFAR10', help='CIFAR10 / MNIST / FashionMNIST / ImageNet')
+    parser.add_argument('--data_path', required=False, default='/userhome/data/cifar10',
+                        help='data path')
+    parser.add_argument('--image_size', type=int, default=32, help='The size of the input Image')
+    parser.add_argument('--batch_size', type=int, default=256, help='Batch size for the data set')
+    # in search phase
+    parser.add_argument('--datset_split', type=int, default=2, help='dataset split')
+    parser.add_argument('--workers', type=int, default=4, help='# of workers')
+
+
+def network_parser(parser):
+    parser.add_argument('--w_lr', type=float, default=0.025, help='lr for weights')
+    parser.add_argument('--w_lr_step', type=int, default=2, help='lr for weights')
+    parser.add_argument('--w_lr_min', type=float, default=0.001, help='minimum lr for weights')
+    parser.add_argument('--w_momentum', type=float, default=0.9, help='momentum for weights')
+    parser.add_argument('--w_weight_decay', type=float, default=3e-4,
+                        help='weight decay for weights')
+    parser.add_argument('--w_grad_clip', type=float, default=5.,
+                        help='gradient clipping for weights')
+    parser.add_argument('--init_channels', type=int, default=16)
+    parser.add_argument('--layers', type=int, default=8, help='# of layers')
+    # node is fixed in most case
+    parser.add_argument('--n_nodes', type=int, default=4, help='# nodes in each cell')
+
+
+def train_parser(parser):
+    parser.add_argument('--print_freq', type=int, default=50, help='print frequency')
+    parser.add_argument('--gpus', default='0', help='gpu device ids separated by comma. '
+                                                    '`all` indicates use all gpus.')
+    parser.add_argument('--epochs', type=int, default=200, help='# of training epochs')
+    # for one shot NAS
+    parser.add_argument('--warm_up_epochs', type=int, default=0, help='# of training epochs')
+    # random seed
+    parser.add_argument('--seed', type=int, default=2, help='random seed')
+
+
+class SearchConfig(BaseConfig):
+
+    def build_parser(self):
+        parser = get_parser("Search config")
+        train_parser(parser)
+        network_parser(parser)
+        dataset_parser(parser)
+        return parser
+
+    def __init__(self):
+        parser = self.build_parser()
+        args = parser.parse_args()
+        super().__init__(**vars(args))
+        time_str = time.asctime(time.localtime()).replace(' ', '_')
+        self.path = os.path.join('experiment', self.name,
+                                 self.sub_name + '_' + time_str)
+        # self.plot_path = os.path.join(self.path, 'plots')
+        self.gpus = parse_gpus(self.gpus)
---
+++ model/darts_cnn.py
@@ -0,0 +1,67 @@
+from models.darts_cell import *
+import utils.genotypes as gt
+import numpy as np
+from models.base_module import MyModule
+
+
+# search cnn
+class SelectSearchCNN(MyModule):
+
+    def __init__(self, C_in=3, C=16, n_classes=10, n_layers=8, n_nodes=4, net_ceri=None):
+        super().__init__()
+        stem_multiplier = 3
+        self.criterion = net_ceri
+        self.C_in = C_in  # 3
+        self.C = C  # 16
+        self.n_classes = n_classes  # 10
+        self.n_layers = n_layers  # 8
+        self.n_nodes = n_nodes  # 4
+        C_cur = stem_multiplier * C  # 3 * 16 = 48
+        self.stem = nn.Sequential(
+            nn.Conv2d(C_in, C_cur, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(C_cur)
+        )
+        # for the first cell, stem is used for both s0 and s1
+        # [!] C_pp and C_p is output channel size, but C_cur is input channel size.
+        C_pp, C_p, C_cur = C_cur, C_cur, C
+        # 48   48   16
+        self.cells = nn.ModuleList()
+        reduction_p = False
+        for i in range(n_layers):
+            # Reduce featuremap size and double channels in 1/3 and 2/3 layer.
+            if i in [n_layers // 3, 2 * n_layers // 3]:
+                C_cur *= 2
+                reduction = True
+            else:
+                reduction = False
+            cell = SelectCell(n_nodes, C_pp, C_p, C_cur, reduction_p, reduction)
+            reduction_p = reduction
+            self.cells.append(cell)
+            C_cur_out = C_cur * n_nodes
+            C_pp, C_p = C_p, C_cur_out
+        self.gap = nn.AdaptiveAvgPool2d(1)
+        self.linear = nn.Linear(C_p, n_classes)
+        # number of edges per cell
+        self.num_edges = sum(list(range(2, self.n_nodes + 2)))
+
+    def forward(self, x, sample):
+        s0 = s1 = self.stem(x)
+
+        for cell in self.cells:
+            weights = sample[self.num_edges:] if cell.reduction else sample[0:self.num_edges]
+            s0, s1 = s1, cell(s0, s1, weights)
+
+        out = self.gap(s1)
+        out = out.view(out.size(0), -1)  # flatten
+        logits = self.linear(out)
+        return logits
+
+    def genotype(self, theta):
+        theta_norm = utils.darts_weight_unpack(theta[0:self.num_edges], self.n_nodes)
+        theta_reduce = utils.darts_weight_unpack(theta[self.num_edges:], self.n_nodes)
+        gene_normal = gt.parse_numpy(theta_norm, k=2)
+        gene_reduce = gt.parse_numpy(theta_reduce, k=2)
+        concat = range(2, 2+self.n_nodes) # concat all intermediate nodes
+
+        return gt.Genotype(normal=gene_normal, normal_concat=concat,
+                           reduce=gene_reduce, reduce_concat=concat)
